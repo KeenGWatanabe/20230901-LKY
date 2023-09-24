b5733b9f8aae5c8ec0ddf00eebab347be3a0f1ca
RgerWeb <mckeenweb@gmail.com>
2023-09-24 13:09:26 +0800

    z

---
+++ list.js
@@ -0,0 +1,7 @@
+function displayAnswer(elementId) {
+    if (elementId === 'java') {
+      alert('This is the answer for "Show Answer".');
+    } else if (elementId === 'works') {
+      alert('This is the answer for "Everything works".');
+    }
+  }
