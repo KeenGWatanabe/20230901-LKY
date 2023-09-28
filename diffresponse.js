28c31fb08bd596e5e7a313ad3275daa0ad195ece
RgerWeb <mckeenweb@gmail.com>
2023-09-28 19:55:40 +0800

    w

--- diffresponse.js
+++ diffresponse.js
@@ -5,12 +5,19 @@
   // Function to handle form submission
   function handleFormSubmission() {
     // Get the value of the input field
-    const inputValue = inputElement.value;
+    const inputText = inputElement.value;
+    if(inputText===""){
+      document.getElementById("answer").innerHTML=`Your input is blank`;
+    }
+    else if(inputText.toLowerCase().indexOf("thank you")>-1){
+			document.getElementById("answer").innerHTML="You are welcome.不用客气。";
+		}
 
     // Update the answer element's innerHTML with the input value
-    answerElement.innerHTML = `Please rephrase your question as lky has a few ${inputValue} in his books.`;
+    else{
+    answerElement.innerHTML = `I do not understand " ${inputText} ", perhaps you could rephrase your question or check your spelling.<br>Or email us what was your question for us to update an appropriate response.<br><a href="mailto:freemindson.co@gmail.com?&subject=ChatBot%20Enquiry&body=Thank%20you%20for%20your%20email%20to%20FreeMindson", target="_blank">email</a>`;
   }
-
+  }
   // Add an event listener to listen for form submission
   document.addEventListener("submit", function(event) {
     event.preventDefault(); // Prevent form submission
