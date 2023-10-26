ff0e81f46d96231b1182c592540e67855fec5b96
60936 <studio34pl@gmail.com>
2023-10-27 06:38:16 +0800

    g

---
+++ list/prompt.js
@@ -0,0 +1,23 @@
+function displayAnswer1(elementId) {
+    if (elementId === 'contents') {
+        window.open(href="list/contents.html",'chat AI','resizable,height=300,width=500');}
+
+else if (elementId === 'announce independence') {document.getElementById("text_a").value = "announce independence";}
+
+else if (elementId === 'reason separate from malaysia') {document.getElementById("text_a").value = "reason separate from malaysia";}
+
+else if (elementId === 'breakdown on tv') {document.getElementById("text_a").value = "breakdown on television";}
+
+else if (elementId === 'was separation planned') {document.getElementById("text_a").value = "was separation planned";}
+
+else if (elementId === 'what was original intent') {document.getElementById("text_a").value = "what was original intent";}
+
+else if (elementId === 'how did people react') {document.getElementById("text_a").value = "how did people react to separation";}
+
+else if (elementId === 'sad') {document.getElementById("text_a").value = "sad";}
+
+
+
+//else if (elementId === '') {document.getElementById("text_a").value = "";}
+
+}
