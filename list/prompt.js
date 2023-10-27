65177074ae3c8ff7143082df941e241a60775103
60936 <studio34pl@gmail.com>
2023-10-27 19:41:56 +0800

    r

--- list/prompt.js
+++ list/prompt.js
@@ -3,96 +3,50 @@ function displayAnswer1(elementId) {
         window.open(href="list/contents.html",'chat AI','resizable,height=300,width=500');}
 
 else if (elementId === 'p1') {document.getElementById("text_a").value = "announce independence";}
-
 else if (elementId === 'p2') {document.getElementById("text_a").value = "reason separate from malaysia";}
-
 else if (elementId === 'p3') {document.getElementById("text_a").value = "breakdown on television";}
-
 else if (elementId === 'p4') {document.getElementById("text_a").value = "was separation planned";}
-
 else if (elementId === 'p5') {document.getElementById("text_a").value = "what was original intent";}
-
 else if (elementId === 'p6') {document.getElementById("text_a").value = "how did people react to separation";}
-
 else if (elementId === 'p7') {document.getElementById("text_a").value = "sad";}
-
 else if (elementId === 'p8') {document.getElementById("text_a").value = "childhood memory";}
-
 else if (elementId === 'p9') {document.getElementById("text_a").value = "your father";}
-
 else if (elementId === 'p10') {document.getElementById("text_a").value = "your parents";}
-
 else if (elementId === 'p11') {document.getElementById("text_a").value = "father's job";}
-
 else if (elementId === 'p12') {document.getElementById("text_a").value = "chinese origin";}
-
 else if (elementId === 'p13') {document.getElementById("text_a").value = "your grandfather";}
-
 else if (elementId === 'p14') {document.getElementById("text_a").value = "grandfather's fortune";}
-
 else if (elementId === 'p15') {document.getElementById("text_a").value = "childhood playing";}
-
 else if (elementId === 'p16') {document.getElementById("text_a").value = "family head";}
-
 else if (elementId === 'p17') {document.getElementById("text_a").value = "primary school";}
-
 else if (elementId === 'p18') {document.getElementById("text_a").value = "secondary school";}
-
 else if (elementId === 'p19') {document.getElementById("text_a").value = "colonial school";}
-
 else if (elementId === 'p20') {document.getElementById("text_a").value = "school canning";}
-
 else if (elementId === 'p21') {document.getElementById("text_a").value = "politics calling";}
-
 else if (elementId === 'p22') {document.getElementById("text_a").value = "cabinet network";}
-
 else if (elementId === 'p23') {document.getElementById("text_a").value = "british coolness";}
-
 else if (elementId === 'p24') {document.getElementById("text_a").value = "trader japanese occupation";}
-
 else if (elementId === 'p25') {document.getElementById("text_a").value = "enterprise japanese occupation";}
-
 else if (elementId === 'p26') {document.getElementById("text_a").value = "partnership japanese occupation";}
-
 else if (elementId === 'p27') {document.getElementById("text_a").value = "scarcity";}
-
 else if (elementId === 'p28') {document.getElementById("text_a").value = "why gukha";}
-
 else if (elementId === 'p29') {document.getElementById("text_a").value = "japanese bully";}
-
 else if (elementId === 'p30') {document.getElementById("text_a").value = "colonial economy";}
-
 else if (elementId === 'p31') {document.getElementById("text_a").value = "describe japanese torture";}
-
 else if (elementId === 'p32') {document.getElementById("text_a").value = "japanese business";}
-
 else if (elementId === 'p33') {document.getElementById("text_a").value = "japanese assets";}
-
 else if (elementId === 'p34') {document.getElementById("text_a").value = "how the war affect you";}
-
 else if (elementId === 'p35') {document.getElementById("text_a").value = "kempeitai";}
-
 else if (elementId === 'p36') {document.getElementById("text_a").value = "comfort women";}
-
 else if (elementId === 'p37') {document.getElementById("text_a").value = "japanese prison";}
-
 else if (elementId === 'p38') {document.getElementById("text_a").value = "japanese punishment";}
-
 else if (elementId === 'p39') {document.getElementById("text_a").value = "japanese torture";}
-
 else if (elementId === 'p40') {document.getElementById("text_a").value = "atom bomb";}
-
 else if (elementId === 'p41') {document.getElementById("text_a").value = "learn japanese";}
-
 else if (elementId === 'p42') {document.getElementById("text_a").value = "banana notes";}
-
-
 else if (elementId === 'p43') {document.getElementById("text_a").value = "met your wife";}
-
 else if (elementId === 'p44') {document.getElementById("text_a").value = "most important years";}
-
 else if (elementId === 'p45') {document.getElementById("text_a").value = "colour prejudice";}
-
 else if (elementId === 'p46') {document.getElementById("text_a").value = "anti-british";}
 else if (elementId === 'p47') {document.getElementById("text_a").value = "british working class";}
 else if (elementId === 'p48') {document.getElementById("text_a").value = "british upper class";}
