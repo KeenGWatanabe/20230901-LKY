f22abf3a119eac931ac8c193923e2cfe6d78e4b9
KeenGWatanabe <mckeenweb@gmail.com>
2023-10-27 11:23:33 +0800

    a

--- list/prompt.js
+++ list/prompt.js
@@ -2,51 +2,77 @@ function displayAnswer1(elementId) {
     if (elementId === 'contents') {
         window.open(href="list/contents.html",'chat AI','resizable,height=300,width=500');}
 
-else if (elementId === 'announce independence') {document.getElementById("text_a").value = "announce independence";}
+else if (elementId === 'p1') {document.getElementById("text_a").value = "announce independence";}
 
-else if (elementId === 'reason separate from malaysia') {document.getElementById("text_a").value = "reason separate from malaysia";}
+else if (elementId === 'p2') {document.getElementById("text_a").value = "reason separate from malaysia";}
 
-else if (elementId === 'breakdown on tv') {document.getElementById("text_a").value = "breakdown on television";}
+else if (elementId === 'p3') {document.getElementById("text_a").value = "breakdown on television";}
 
-else if (elementId === 'was separation planned') {document.getElementById("text_a").value = "was separation planned";}
+else if (elementId === 'p4') {document.getElementById("text_a").value = "was separation planned";}
 
-else if (elementId === 'what was original intent') {document.getElementById("text_a").value = "what was original intent";}
+else if (elementId === 'p5') {document.getElementById("text_a").value = "what was original intent";}
 
-else if (elementId === 'how did people react') {document.getElementById("text_a").value = "how did people react to separation";}
+else if (elementId === 'p6') {document.getElementById("text_a").value = "how did people react to separation";}
 
-else if (elementId === 'sad') {document.getElementById("text_a").value = "sad";}
+else if (elementId === 'p7') {document.getElementById("text_a").value = "sad";}
 
-else if (elementId === 'childhood') {document.getElementById("text_a").value = "childhood memory";}
+else if (elementId === 'p8') {document.getElementById("text_a").value = "childhood memory";}
 
-else if (elementId === 'father') {document.getElementById("text_a").value = "your father";}
+else if (elementId === 'p9') {document.getElementById("text_a").value = "your father";}
 
-else if (elementId === 'parents') {document.getElementById("text_a").value = "your parents";}
+else if (elementId === 'p10') {document.getElementById("text_a").value = "your parents";}
 
-else if (elementId === 'father job') {document.getElementById("text_a").value = "father's job";}
+else if (elementId === 'p11') {document.getElementById("text_a").value = "father's job";}
 
-else if (elementId === 'chinese origin') {document.getElementById("text_a").value = "chinese origin";}
+else if (elementId === 'p12') {document.getElementById("text_a").value = "chinese origin";}
 
-else if (elementId === 'grandfather') {document.getElementById("text_a").value = "your grandfather";}
+else if (elementId === 'p13') {document.getElementById("text_a").value = "your grandfather";}
 
-else if (elementId === 'grandfather fortune') {document.getElementById("text_a").value = "grandfather's fortune";}
+else if (elementId === 'p14') {document.getElementById("text_a").value = "grandfather's fortune";}
 
-else if (elementId === 'childhood play') {document.getElementById("text_a").value = "childhood playing";}
+else if (elementId === 'p15') {document.getElementById("text_a").value = "childhood playing";}
 
-else if (elementId === 'family head') {document.getElementById("text_a").value = "family head";}
+else if (elementId === 'p16') {document.getElementById("text_a").value = "family head";}
 
-else if (elementId === 'pri sch') {document.getElementById("text_a").value = "primary school";}
+else if (elementId === 'p17') {document.getElementById("text_a").value = "primary school";}
 
-else if (elementId === 'sec sch') {document.getElementById("text_a").value = "secondary school";}
+else if (elementId === 'p18') {document.getElementById("text_a").value = "secondary school";}
 
-else if (elementId === 'colonial sch') {document.getElementById("text_a").value = "colonial school";}
+else if (elementId === 'p19') {document.getElementById("text_a").value = "colonial school";}
 
-else if (elementId === 'sch canning') {document.getElementById("text_a").value = "school canning";}
+else if (elementId === 'p20') {document.getElementById("text_a").value = "school canning";}
 
-else if (elementId === 'politics calling') {document.getElementById("text_a").value = "politics calling";}
+else if (elementId === 'p21') {document.getElementById("text_a").value = "politics calling";}
 
-else if (elementId === 'cabinet network') {document.getElementById("text_a").value = "cabinet network";}
+else if (elementId === 'p22') {document.getElementById("text_a").value = "cabinet network";}
 
-else if (elementId === 'british coolness') {document.getElementById("text_a").value = "british coolness";}
+else if (elementId === 'p23') {document.getElementById("text_a").value = "british coolness";}
+
+else if (elementId === 'p24') {document.getElementById("text_a").value = "trader japanese occupation";}
+
+else if (elementId === 'p25') {document.getElementById("text_a").value = "enterprise japanese occupation";}
+
+else if (elementId === '') {document.getElementById("text_a").value = "";}
+
+else if (elementId === '') {document.getElementById("text_a").value = "";}
+
+else if (elementId === '') {document.getElementById("text_a").value = "";}
+
+else if (elementId === '') {document.getElementById("text_a").value = "";}
+
+else if (elementId === '') {document.getElementById("text_a").value = "";}
+
+else if (elementId === '') {document.getElementById("text_a").value = "";}
+
+else if (elementId === '') {document.getElementById("text_a").value = "";}
+
+else if (elementId === '') {document.getElementById("text_a").value = "";}
+
+else if (elementId === '') {document.getElementById("text_a").value = "";}
+
+else if (elementId === '') {document.getElementById("text_a").value = "";}
+
+else if (elementId === '') {document.getElementById("text_a").value = "";}
 
 //else if (elementId === '') {document.getElementById("text_a").value = "";}
 
